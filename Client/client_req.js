cb953afc0b0b1714bad463c537a515407ac0acbf
Thomas Nakache <nakach_t@etna-alternance.net>
2017-10-11 13:54:56 +0200

    génération de l'id aléatoire.

--- Client/client_req.js
+++ Client/client_req.js
@@ -2,39 +2,39 @@ var zmq = require('zmq')
     , sock = zmq.socket('req');
 
 
-// var commands = {
-//     "forward":"",
-//     "backward":"",
-//     "leftfwd":"",
-//     "rightfwd":"",
-//     "right":"",
-//     "left":"",
-//     "looking":"",
-//     "gather":"",
-//     "watch":"",
-//     "attack":"",
-//     "selfid":"",
-//     "selfstats":"",
-//     "inspect":"#0x01",
-//     "next":"",
-//     "jump":"",
-//     "identify":"#0x1"
-// };
-
 var commands = {
-    "identify":"#0x1",
-    "identify":"#0x2",
-    "identify":"#0x3",
-    "identify":"#0x4"
+    "forward":"",
+    "backward":"",
+    "leftfwd":"",
+    "rightfwd":"",
+    "right":"",
+    "left":"",
+    "looking":"",
+    "gather":"",
+    "watch":"",
+    "attack":"",
+    "selfid":"",
+    "selfstats":"",
+    "inspect":"",
+    "next":"",
+    "jump":"",
+    "identify":""
 };
 
+
+
+function randomid(){
+    var txt = "#0x";
+    var nbr = Math.floor(Math.random() * 100000) + 1
+    return txt+nbr;
+}
 
 function randomKey(obj) {
     var ret;
     var c = 0;
     for (var key in obj)
         if (Math.random() < 1/++c)
-	    ret = key;
+        ret = key;
     return ret;
 }
 
@@ -44,30 +44,24 @@ function randomKey(obj) {
 var x = 0;
 sock.connect("tcp://localhost:5555");
 sock.on("message", function(reply) {
-	console.log("Received reply", x, ": [", reply.toString(), ']');
-	x += 1;
-	if (x === 10) {
-	    sock.close();
-	    process.exit(0);
-	}
-    });
+    console.log("Received reply", x, ": [", reply.toString(), ']');
+    x += 1;
+    if (x === 10) {
+        sock.close();
+        process.exit(0);
+    }
+});
 
+console.log("Sending 10 request...");
 for (var i = 0; i < 10; i++) {
     var rdk = randomKey(commands);
-    console.log("Sending request", i, '…');
-    sock.send(rdk+"|"+commands[rdk]);
+    if (rdk == "identify" || rdk == "inspect") {
+        sock.send(rdk+"|"+randomid());
+    } else {
+        sock.send(rdk+"|"+commands[rdk]);
+    };
 }
 
-// function myFunction() {
-//     setTimeout(function(){ 
-//         var rdk = randomKey(commands);
-//         console.log("Sending request…");
-//         sock.send(rdk+"|"+commands[rdk]);
-//         myFunction(); 
-//     }, 1000);
-// }
-// myFunction();
-
 process.on('SIGINT', function() {
-	sock.close();
-	});
+    sock.close();
+    });
